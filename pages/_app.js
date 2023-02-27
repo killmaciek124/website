004c9fc72ee6d7b504b3a9a621d8c90b9a1ce7c8
Maciej <maciej@maziuk.com.pl>
2023-02-27 14:42:33 +0100

    /pages add redux and Layout to root component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Layout from "@/components/layout/Layout";
+import store from "@/store";
+import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  );
 }
