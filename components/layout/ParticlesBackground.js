763af69c09f97ecbf96027e74ad3ee41e69d630c
Maciej <maciej@maziuk.com.pl>
2023-02-27 14:37:11 +0100

    /layout add particles background

---
+++ components/layout/ParticlesBackground.js
@@ -0,0 +1,24 @@
+import classes from "./ParticlesBackground.module.css";
+import Particles from "react-tsparticles";
+import particlesConfig from "../config/particles-config";
+import { loadFull } from "tsparticles";
+
+const ParticlesBackground = () => {
+  const particlesInit = async (main) => {
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    // to do: note using your words.
+    await loadFull(main);
+  };
+
+  return (
+    <Particles
+      className={classes.particles}
+      init={particlesInit}
+      params={particlesConfig}
+    ></Particles>
+  );
+};
+
+export default ParticlesBackground;
