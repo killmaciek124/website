8778b0bd7ebbf9994a1ea4d4409192e4c95fa4ba
Maciej <maciej@maziuk.com.pl>
2023-02-27 14:45:12 +0100

    auto generated code

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+// ZROB SEKRETNEGO ENDPOINTA Z KOTKIEM I STRINGIEM GRaTULACJE cos tam i url kotka
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: "John Doe" });
 }
