6c61fc50b1a27d95d3911055581fd6eb43a6da68
Maciej <maciej@maziuk.com.pl>
2023-02-27 14:41:09 +0100

    /layout add sideNav for mobiles

---
+++ components/layout/SideBar.js
@@ -0,0 +1,39 @@
+import classes from "./SideBar.module.css";
+import { FiX } from "react-icons/fi";
+import { navActions } from "@/store/index";
+import { useDispatch } from "react-redux";
+import Link from "next/link";
+
+const SideBar = () => {
+  const dispatch = useDispatch();
+  const onHideBarHandler = () => {
+    dispatch(navActions.hideNavBar());
+  };
+
+  return (
+    <nav className={classes.navbar}>
+      <label onClick={onHideBarHandler}>
+        <p>
+          <FiX />
+        </p>
+      </label>
+
+      <div className={classes.menu}>
+        <ul className={classes.list}>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+
+          <li>
+            <Link href="/projects">Projects</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default SideBar;
