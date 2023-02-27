7ce257750a47a7e9a4eb69db4522e35c95513528
Maciej <maciej@maziuk.com.pl>
2023-02-27 14:39:21 +0100

    /layout add single project component

---
+++ components/layout/Project.js
@@ -0,0 +1,36 @@
+import classes from "./Project.module.css";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useDispatch } from "react-redux";
+import { projectActions } from "@/store";
+
+const Project = (props) => {
+  const router = useRouter();
+  const dispatch = useDispatch();
+
+  const seeMoreButtonHandler = () => {
+    dispatch(projectActions.clearState());
+
+    dispatch(
+      projectActions.setState({
+        title: props.title,
+        description: props.description,
+      })
+    );
+    router.push("/projects/" + props.id);
+  };
+
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.container}>
+        <div onClick={seeMoreButtonHandler} className={classes.button}>
+          See more
+        </div>
+        <Image className={classes.img} src={props.img} alt={props.alt} />
+      </div>
+      <h2>{props.title}</h2>
+    </div>
+  );
+};
+
+export default Project;
