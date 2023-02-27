ba8f33922014d434194c80e30373c52d681d8eaa
Maciej <maciej@maziuk.com.pl>
2023-02-27 14:31:25 +0100

    /layout add footer with icons

---
+++ components/layout/Footer.js
@@ -0,0 +1,35 @@
+import classes from "./Footer.module.css";
+import { BsLinkedin, BsGithub, BsYoutube } from "react-icons/bs";
+import { BiCopyright } from "react-icons/bi";
+
+const Footer = (props) => {
+  const footerClass = props.class;
+  return (
+    <footer className={classes[footerClass]}>
+      <section className={classes.links}>
+        <a
+          href="https://www.linkedin.com/in/maciej-maziuk-120ab1242/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+        <a href="https://github.com/killmaciek124" target="_blank">
+          <BsGithub />
+        </a>
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">
+          <BsYoutube />
+        </a>
+      </section>
+      <ul className={classes.info}>
+        <li>Maciej Maziuk</li>
+        <li>
+          <BiCopyright />
+          2023
+        </li>
+        <li>Maziuk's Website</li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
